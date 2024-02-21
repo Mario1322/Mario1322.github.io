3dfe5ed5409f4d1dc1bf3d31138158fd5a579291
Mario1322 <mariodelarosagr2003@gmail.com>
2024-02-21 19:36:09 +0100

    V16
    cambios en el desplazamiento

--- js/script.js
+++ js/script.js
@@ -5,27 +5,27 @@ let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active')
-}
-
-
+    navbar.classList.toggle('active');
+};
 
 // Scroll a la sección seleccionada
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let fromTop = window.scrollY;
+
     sections.forEach(sec => {
-        let top = window.scrollY;
+        let sectionId = sec.getAttribute('id');
+        let navLink = document.querySelector('header nav a[href="#' + sectionId + '"]');
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
+        if (fromTop >= offset && fromTop < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
             });
-            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+            navLink.classList.add('active');
         }
     });
 
