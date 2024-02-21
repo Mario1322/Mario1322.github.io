ade59c2b5b5390fbad90646ddc427a33088f9468
Mario1322 <mariodelarosagr2003@gmail.com>
2024-02-21 19:22:29 +0100

    V14
    modificación para redirección

--- js/script.js
+++ js/script.js
@@ -10,10 +10,9 @@ menuIcon.onclick = () => {
 
 
 
-/* Scroll a la sección seleccionada */
-
+// Scroll a la sección seleccionada
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a')
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -25,8 +24,8 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
             });
+            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
         }
     });
 
@@ -35,9 +34,8 @@ window.onscroll = () => {
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+};
 
-
-}
 
 /* Cambio de tema */
 let toggle = document.getElementById('toggle');
