89608af952cb9b22a0cd708fee430182fe139521
Mario1322 <mariodelarosagr2003@gmail.com>
2025-08-01 03:10:42 +0200

    V79
    He creado la pagina indexen , lo que hace que la pagina web se encuentre en español y en ingles , todo el texto ya esta traducido .
    
    Tambien hay cambios en la estetica de la pagina web, como por ejemplo el boton de descarga de Certificado , ahora ya se parece a los del resto de la pagina web

--- js/maquinaescribir.js
+++ js/maquinaescribir.js
@@ -1,6 +1,13 @@
 // typingEffect.js
 
-const texto = "Soy un joven apasionado con un constante anhelo de aprender y expresar mi sabiduría. Mi sed de conocimiento me impulsa a explorar continuamente nuevas tecnologías y herramientas, buscando mejorar y perfeccionar mis habilidades en cada paso del camino.";
+const textoES = "Soy un joven apasionado con un constante anhelo de aprender y expresar mi sabiduría. Mi sed de conocimiento me impulsa a explorar continuamente nuevas tecnologías y herramientas, buscando mejorar y perfeccionar mis habilidades en cada paso del camino.";
+
+const textoEN = "I am a young man passionate about learning and expressing my knowledge. My thirst for knowledge drives me to continuously explore new technologies and tools, seeking to improve and refine my skills every step of the way.";
+
+// Detecta si estás en el index en inglés (por ejemplo: index-en.html)
+const esIngles = window.location.pathname.includes("indexen");
+
+const texto = esIngles ? textoEN : textoES;
 const output = document.getElementById("textosobremi");
 let index = 0;
 
@@ -12,7 +19,6 @@ function typeWriter() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM completamente cargado');
+document.addEventListener('DOMContentLoaded', function () {
     typeWriter();
 });
